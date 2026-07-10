5c15e134276f34d8cf05d940cef6449ceacb58f0
puddinging2209 <jinjin2008jp@gmail.com>
2026-07-11 07:27:29 +0900

    色を変更

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function repplaceRelativeTime() {
-    const bgColor = $($('.State')[0]).css('background-color') || '#00a497';
-    $('relative-time').each(function () {
-        const absTime = moment($(this).attr('datetime')).format('L LT');
-        const height = $(this).height();
-        this.shadowRoot.innerHTML = absTime
-        $(this).css({ color: bgColor, borderRadius: `${height / 2}px`, padding: '2px 4px', fontSize: 'smaller' });
-    });
+	const bgColor = $($('.State')[0]).css('background-color') || '#303030';
+	$('relative-time').each(function () {
+		const absTime = moment($(this).attr('datetime')).format('L LT');
+		const height = $(this).height();
+		this.shadowRoot.innerHTML = absTime;
+		$(this).css({ color: bgColor, borderRadius: `${height / 2}px`, padding: '2px 4px', fontSize: 'smaller' });
+	});
 }
 setTimeout(repplaceRelativeTime, 0.5 * 1000);
 setInterval(repplaceRelativeTime, 1 * 1000);
